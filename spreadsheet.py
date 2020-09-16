7128b5f3e111f95cb13696d9a2409feec5a5b804
Ketaki Deuskar <ketakideuskar@ketakis-mbp.lan>
2020-09-15 23:11:23 -0700

    night commit

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,1 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
 
+scope = ['https://spreadsheets.google.com/feeds' + ' ' +'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet_1 = client.open('Test_Little').sheet1
+sheet_1.update_cell(1, 1, "ketaki")
