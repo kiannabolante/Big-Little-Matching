f006b3c8d73f5fbf45cdb47e0b79c85478f5a16a
Ketaki Deuskar <ketakideuskar@ketakis-mbp.lan>
2020-09-22 16:10:23 -0700

    filters based on class standing and classes taken

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,10 +1,43 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from collections import OrderedDict
 import pprint
 
 scope = ['https://spreadsheets.google.com/feeds' + ' ' +'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-sheet_1 = client.open('Test_Little').sheet1
-sheet_1.update_cell(1, 1, "ketaki")
+little_sheet = client.open('Test CSE Little Sign Up (Autumn 2020) (Responses)').sheet1
+big_sheet = client.open('Test Big Sign Up (Autumn 2020) (Responses)').sheet1
+matches = client.open('Testing Matches').sheet1
+
+littles = little_sheet.get_all_values()
+bigs = big_sheet.get_all_values()
+
+d = {}
+
+mapping = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
+
+class_standings = ["Freshman", "Sophomore", "Junior", "Senior", "Other"]
+
+# for each little
+#   add them to the mapping
+#   for each big
+#       if big's classes > little's classes
+#           add them to the mapping's value
+for little in littles[1:len(littles)]:
+    key = little[7]
+    mapping[key] = []
+    for big in bigs[1:len(bigs)]:
+        # comparing class standing
+        if ((class_standings.index(big[8]) - class_standings.index(little[8])) > 0):
+            # comparing classes
+            if (len(big[12]) > len(little[12])):
+                mapping[key].append(big[7])
+    print(little[8])
+    print(mapping[key])
+    print()
+        
+    
+
+
