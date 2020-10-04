c27ea3d7b04f96b94b126d35ad8111931f95c634
Ketaki Deuskar <ketakideuskar@Ketakis-MBP.hsd1.wa.comcast.net>
2020-10-03 19:32:53 -0700

    separated into methods

--- spreadsheet.py
+++ spreadsheet.py
@@ -3,6 +3,39 @@ from oauth2client.service_account import ServiceAccountCredentials
 from collections import OrderedDict
 import pprint
 
+def matchBack(little_background, big_background):
+    points = 0
+    for little_back in little_background:
+        for big_back in big_background:
+            if (little_back == big_back):
+                points += 3
+    return points
+
+def matchLiving(little_living, big_living):
+    points = 0
+    for little_liv in little_living:
+        for big_liv in big_living:
+            if (little_liv == big_liv):
+                points += 2
+    return points
+
+def matchRace(little_race, big_race):
+    points = 0
+    for lit_race in little_race:
+        for b_race in big_race:
+            if (lit_race == b_race):
+                points += 2
+    return points
+
+def matchInterests(little_interests, big_interests):
+    points = 0
+    for little_int in little_interests:
+        for big_int in big_interests:
+            if (little_int == big_int):
+                points += 1
+    return points
+
+
 scope = ['https://spreadsheets.google.com/feeds' + ' ' +'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
@@ -16,14 +49,11 @@ bigs = big_sheet.get_all_values()
 
 bigs_left = {""}
 
-# add all bigs to bigs left set
+# add all bigs to bigs-left set
 for big in bigs[1:len(bigs)]:
     bigs_left.add(big[7])
 
 bigs_left.discard("")
-
-print(bigs_left)
-print()
 
 d = {}
 
@@ -35,7 +65,7 @@ worksheet = matches.get_worksheet(1)
 
 row = 1 # update for each little
 
-# matching
+# matching each little 
 for little in littles[1:len(littles)]:
     key = little[7]
     mapping[key] = []
@@ -52,48 +82,43 @@ for little in littles[1:len(littles)]:
         if ((class_standings.index(big[8]) - class_standings.index(little[8])) > 0):    # might have to change this
             # comparing number of classes taken
             if (len(big[12]) > len(little[12])):
-                # comparing hours
-             #   if (big[10] == little[10]):
-                    # match identities
                     big_living = big[14].split(", ")
                     big_background = big[13].split(", ")
                     big_race = big[17].split(", ")
-                    big_interests = big[19].split(", ")
-                    for little_back in little_background:
-                        for big_back in big_background:
-                            if (little_back == big_back):
-                                points += 3
+                    big_interests = big[19].split(", ")    
+                    
+                    # match identities
+                    points += matchBack(little_background, big_background)
+                    
                     # match living situation
-                    for lit_living in little_living:
-                        for b_living in big_living:
-                            if (lit_living == b_living):
-                                points += 2
+                    points += matchLiving(little_living, big_living)
+                    
                     # match pronouns
                     if (big[15] == little[15]): # hesitant on adding these
                         points += 2
+                    
                     # match sexuality
                     if (big[16].lower() == little[16].lower()):
                         points += 2
+                    
                     # match race
-                    for lit_race in little_race:
-                        for b_race in big_race:
-                            if (lit_race == b_race):
-                                points += 2
+                    points += matchRace(little_race, big_race)
+                    
                     # match interests
-                    for little_int in little_interests:
-                        for big_int in big_interests:
-                            if (little_int == big_int):
-                                points += 1
+                    points += matchInterests(little_interests, big_interests)
+                    
                     # add big to map
                     if (points >= 11):
                        big_list.append([points, big[7]])
                        bigs_left.discard(big[7])
+
     sorted_list = sorted(big_list, key=lambda x: x[0], reverse=True)  
     for big in sorted_list:
-        mapping[key].append(big[0])
-        mapping[key].append(big[1])
+        mapping[key].append(big[0]) # points
+        mapping[key].append(big[1]) # cse email
     # adding bigs to spreadsheet
     values = mapping[key]
+    # google sheets goes from A-Z
     if (len(values) > 25):
         values = values[0:24]
 
@@ -116,10 +141,3 @@ if (len(little_list) > 0):
     worksheet.update_cells(little_list)
 
 print(bigs_left)
-
-
-
-
-    
-
-
